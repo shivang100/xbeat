503ec166e323d829c48e52b4ddaa1259456a1c61
Kshitij Agarwal <agarwal.kshitij01@gmail.com>
2023-10-04 17:22:30 +0530

    updated

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,3 +1,22 @@
+import {
+    AiOutlineSearch,
+    AiOutlineUser,
+    AiOutlineShoppingCart,
+} from "react-icons/ai";
+import Link from "next/link";
+
 export default function Header() {
-    return <div></div>;
+    return (
+        <header className="h-14 bg-black flex justify-between text-gray-400 text-3xl">
+            <Link href="/">
+                <h1 className="font-bold pl-4 pt-4">X-Beat</h1>
+            </Link>
+
+            <div className="flex gap-16 pt-4 pr-3">
+                <AiOutlineSearch />
+                <AiOutlineShoppingCart />
+                <AiOutlineUser />
+            </div>
+        </header>
+    );
 }
